2b23d721d93af645bd4928d6f3fffe493269e539
rsperoni <rsperoni@mgcoders.com>
2017-11-22 18:09:11 -0300

    Logo

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,13 +5,14 @@ function makeAppConfig() {
 
   const AppConfig = {
     brand: 'MARQ',
+    brandLogoUrl: 'assets/images/logoMarq.png',
     user: 'Lisa',
     year,
     layoutBoxed: false,               // true, false
     navCollapsed: false,              // true, false
-    navBehind: false,                 // true, false
+    navBehind: true,                 // true, false
     fixedHeader: true,                // true, false
-    sidebarWidth: 'small',           // small, middle, large
+    sidebarWidth: 'middle',           // small, middle, large
     theme: 'light',                   // light, gray, dark
     colorOption: '21',                // 11,12,13,14,15,16; 21,22,23,24,25,26; 31,32,33,34,35,36
     AutoCloseMobileNav: true,         // true, false. Automatically close sidenav on route change (Mobile only)
