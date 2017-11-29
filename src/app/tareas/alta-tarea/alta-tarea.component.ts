9b5f5c0f2e05433f708c2e290b4c62fabb0e743e
Cristian Bauza <cristian.bauza.silva@gmail.com>
2017-11-29 16:28:46 -0300

    Dialog

--- src/app/tareas/alta-tarea/alta-tarea.component.ts
+++ src/app/tareas/alta-tarea/alta-tarea.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { DialogOverviewExampleDialogComponent } from '../../ui/components/dialog-overview-example-dialog.component';
 
 @Component({
   selector: 'app-alta-tarea',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltaTareaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+    
   }
-
 }
